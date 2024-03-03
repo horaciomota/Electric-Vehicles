d633bcd5e33839a2ea31a2deddd50557acdcb892
Horacio Mota <horaciomota93@gmail.com>
2024-03-03 18:16:16 +0000

    Delete custom local fonts folder

--- src/Components/CarsDetails.jsx
+++ src/Components/CarsDetails.jsx
@@ -1,25 +1,23 @@
-import '../Styles/CarsDetails/CarsDetails.modules.scss'
+// CarDetails.js
 
-export default function CarDetails() {
+import React from 'react';
+import '../Styles/CarsDetails/CarsDetails.modules.scss';
+
+export default function CarDetails({ title, monthlyFee, unlockFee, perMinute, perHour, perDay, collisionDamage, rentalInfo }) {
     return (
-        <>
         <section className="CarDetails-section">
             <div className="container">
-
-        <div className="carCard">
-        <h1>Yoyo</h1>
-        <p>$0 monthly fee</p>
-        <p>$1.50 to unlock</p>
-        <p>$0.70 per minute</p>
-        <p>$35 per hour</p>
-        <p>$175 per day(24 hrs)</p>
-        <span>Optional $3 per hour collision damage waiver, capped at $15 per day</span>
-        <span>Rentals will not exceed $35 per hour or $175 per day (excluding the optional collision damage waiver). Rentals that exceed 5 hrs will be charged the daily rate.</span>
-        </div>
-
+                <div className="carCard">
+                    <h1>{title}</h1>
+                    <p>${monthlyFee} monthly fee</p>
+                    <p>${unlockFee} to unlock</p>
+                    <p>${perMinute} per minute</p>
+                    <p>${perHour} per hour</p>
+                    <p>${perDay} per day (24 hrs)</p>
+                    <span>{collisionDamage}</span>
+                    <span>{rentalInfo}</span>
+                </div>
             </div>
         </section>
-        
-        </>
     )
 }
