0746a530e51bd19f345d18eb93c388b6876e91a2
Horacio Mota <horaciomota93@gmail.com>
2024-03-03 18:16:38 +0000

    Refactore card detail component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,16 @@ import CarDetails from "@/Components/CarsDetails"
 export default function Home() {
     return (
         <>
-        <CarDetails/>
+        <CarDetails
+        title="Yoyo"
+        monthlyFee="0"
+        unlockFee="1.50"
+        perMinute="0.70"
+        perHour="35"
+        perDay="175"
+        collisionDamage="Optional $3 per hour collision damage waiver, capped at $15 per day"
+        rentalInfo="Rentals will not exceed $35 per hour or $175 per day (excluding the optional collision damage waiver). Rentals that exceed 5 hrs will be charged the daily rate."
+        />
         </>
     )
 }
