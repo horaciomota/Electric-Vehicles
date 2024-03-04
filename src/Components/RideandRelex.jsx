a66e51bba4d1a64b5e33f314c40858b0ea45fcd4
Horacio Mota <horaciomota93@gmail.com>
2024-03-04 22:07:58 +0000

    Ride and relex section added, mobile version working, pink color added and variables need to be implemented

--- src/Components/RideandRelex.jsx
+++ src/Components/RideandRelex.jsx
@@ -1,11 +1,14 @@
+import '../Styles/RideandRelax/RideandRelax.modules.scss'
 
 export default function RideandRelax() {
     return(
         <>
         <div className="Ride-section">
         <div className="container">
-            <h1>teste</h1>
+        <div className='ride-txt'>
+        <h1>Ride and relax</h1>
         <h3>Our clever app always calculates the best fare for the time you've used. For more information, read our handy guide to pricing.</h3>
+        </div>
         </div>
         </div>
         </>
