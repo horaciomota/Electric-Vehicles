c1269b349bddb63e790453dcf6a8a4600cb72ea5
Horacio Mota <horaciomota93@gmail.com>
2024-03-04 21:48:15 +0000

    Refactored CarDetails module with height auto

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import CarDetails from "@/Components/CarsDetails"
+import RideandRelax from "@/Components/RideandRelex"
 
 export default function Home() {
     return (
         <>
         <CarDetails/>
+        <RideandRelax/>
         </>
     )
 }
