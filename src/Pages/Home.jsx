695e7ac070602fab3af8b2ec39472731d6d1d36f
Horacio Mota <horaciomota93@gmail.com>
2024-03-05 20:54:24 +0000

    Pink Wave added, img bg put as cover and its now resizable

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import CarDetails from "@/Components/CarsDetails"
 import RideandRelax from "@/Components/RideandRelex"
+import PinkWave from "@/Components/PinkWave"
 
 export default function Home() {
     return (
         <>
+        <PinkWave/>
         <CarDetails/>
         <RideandRelax/>
         </>
