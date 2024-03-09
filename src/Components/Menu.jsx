8ad35253beb742bb30bb7295a8d4b5efd398e3f1
Horacio Mota <horaciomota93@gmail.com>
2024-03-09 22:19:28 +0000

    Adds QR code component to the menu component, component colapses on mobile version

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import '../Styles/Menu/Menu.modules.scss';
 import logo from "../Images/logo.png";
+import qrcode from "../Images/qrcode.png";
 
 function Menu() {
     return (
         <div className="menu">
-            <img src={logo.src} alt="Logo" className="logo" />
-            <ul className="menu-items">
-                <li><a href="#how-it-works">How it works</a></li>
-                <li><a href="#vehicles">Vehicles</a></li>
-                <li><a href="#apps">Apps</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
+        <img src={logo.src} alt="Logo" className="logo" />
+        <ul className="menu-items">
+            <li><a href="#how-it-works">How it works</a></li>
+            <li><a href="#vehicles">Vehicles</a></li>
+            <li><a href="#apps">Apps</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+        <div className='qrcode-container'>
+        <img src={qrcode.src} alt="Get the app" className="qrcode" />
+        <span className="qrcode-label">Get the app</span>
         </div>
+    </div>
     );
 }
 
