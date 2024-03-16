05da3215621af8a29260559647d6ffe7f203d8d8
Horacio Mota <horaciomota93@gmail.com>
2024-03-16 14:00:03 +0000

    Style: Finishes mobile version for footer section and fix render bug on 800px screens

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,7 @@
 import instagramLogo from "../Images/InstagramLogo.png"
 import footerline from "../Images/footer-line-pink-back 1.png"
 import frame from "../Images/Frame.png"
+import character from "../Images/character.png"
 
 import '../Styles/Footer/Footer.modules.scss'
 
@@ -13,12 +14,16 @@ export default function Footer() {
     </div>
     <div className="footer">
         <div className="container">
+        <div className="footer-bigger-menu">
             <h1>How it works</h1>
             <h1>Vehicles</h1>
             <h1>Apps</h1>
             <h1>Contact</h1>
-
-            <div className='footer-menu'>
+        </div>
+        <img src={character.src} alt="character on footer section" className="character-footer"/>
+        </div>
+        <div className="container">
+        <div className='footer-menu'>
                 <img src={instagramLogo.src} alt="Instagram Logo" className="first-item" />
                <div className="footer-list">
                 <p>Terms & conditions</p>
@@ -27,7 +32,6 @@ export default function Footer() {
                 <span>Current Vehicles Ltd. All rights reserved © 2022</span>
                 </div>
             </div>
-
         </div>
     </div>
     </>
